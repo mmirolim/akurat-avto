28bdcaf0ac4bc9279b28e9a7e0c7063520241b8d
Mirolim Mirzakhmedov <mirolim777@gmail.com>
2014-01-02 18:39:14 +0500

    Init commit

---
+++ app/config/config.php
@@ -0,0 +1,20 @@
+<?php
+
+return new \Phalcon\Config(array(
+	'database' => array(
+		'adapter'     => 'Mysql',
+		'host'        => 'localhost',
+		'username'    => 'akurat_db_user',
+		'password'    => 'x4CNdrnGn4YdnULZ',
+		'dbname'      => 'akurat_avto',
+	),
+	'application' => array(
+		'controllersDir' => __DIR__ . '/../../app/controllers/',
+		'modelsDir'      => __DIR__ . '/../../app/models/',
+		'viewsDir'       => __DIR__ . '/../../app/views/',
+		'pluginsDir'     => __DIR__ . '/../../app/plugins/',
+		'libraryDir'     => __DIR__ . '/../../app/library/',
+		'cacheDir'       => __DIR__ . '/../../app/cache/',
+		'baseUri'        => '/akurat-avto/',
+	)
+));
