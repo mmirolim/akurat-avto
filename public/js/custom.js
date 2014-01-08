8ec5bbfc662ca8f88a63fd6ad409d507cb9d50e3
Mirolim Mirzakhmedov <mirolim777@gmail.com>
2014-01-08 22:45:47 +0500

    Refactor updateCarData() js

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,18 +1,34 @@
-function updateCarData(el) {
-	//TODO clean code add checks and update via ajax
-	var param = el.attr('class');
-	if (el.hasClass("active")) {return}
-	var carId = el.parent().parent().attr('data-car-id');
-	var data = el[0].textContent;
-	var form = '<form class="form-inline-car-update" method="post" action="/cars/updateOwn">';
-	form +='<input name="id" id="id" value="'+ carId +'" type="hidden">';
-	form +='<input name="'+ param +'" id="'+ param +'" value="'+ data +'" type="text">';
-	form +='<input class="button small inline-update-button" value="update" type="submit">';
-	form +='</form>';
-	console.log(form);
-	el[0].innerHTML = form;
-	el.addClass('active');
-
+function updateCarData() {
+    //TODO clean code add checks and update via ajax
+    //Prepare array with target elements
+    var targets = ['dailymilage','milage'];
+    for (var i = 0; i < targets.length; i++) {
+        var els = document.getElementsByClassName(targets[i]);
+        //Check if elements present
+        if(els.length === 0) {return}
+        for (var j = 0; j < els.length; j++) {
+            //AddEventListeners to target elements
+            els[j].addEventListener("click", function(event){
+                var el = event.target;
+                var param = el.getAttribute('class');
+                //Don't insert form twice if element activated
+                if (el.getAttribute("active") === 'yes') {return}
+                //Get car id
+                var carId = el.parentNode.parentNode.getAttribute('data-car-id');
+                //Get current property's value
+                var data = el.textContent;
+                //Prepare form
+                var form = '<form class="form-inline-car-update" method="post" action="/cars/updateOwn">';
+                form +='<input name="id" id="id" value="'+ carId +'" type="hidden">';
+                form +='<input name="'+ param +'" id="'+ param +'" value="'+ data +'" type="text">';
+                form +='<input class="button small inline-update-button" value="update" type="submit">';
+                form +='</form>';
+                //Insert form to inside of target element
+                el.innerHTML = form;
+                el.setAttribute('active','yes');
+            });
+        }
+    }
 }
 
 $(window).load(function(){
@@ -21,4 +37,5 @@ $(window).load(function(){
 	controlNav: false,
 	directionNav: false,
 	})
+    updateCarData();
 });
--- public/js/dev_js/AK.updateCarData.js
+++ public/js/dev_js/AK.updateCarData.js
@@ -10,17 +10,31 @@ AK.updateCarData = function () {
 
 }
 
-function updateCarData(el) {
-	
-	var param = el.attr('class');
-	if (el.hasClass("active")) {return}
-	var data = el[0].textContent;
-	var form = '<form class="form-inline-car-update" method="post" action="/cars/updateOwn">';
-	form +='<input name="'+ param +'" id="'+ param +'" value="'+ data +'" type="text">';
-	form +='<input class="button small inline-update-button" value="update" type="submit">';
-	form +='</form>';
-	console.log(form);
-	el[0].innerHTML = form;
-	el.addClass('active');
+function updateCarData() {
+
+	//TODO clean code add checks and update via ajax
+    var targets = ['dailymilage','milage'];
+    for (var i = 0; i < targets.length; i++) {
+        var els = document.getElementsByClassName(targets[i]);
+        if(els.length === 0) {return}
+        for (var j = 0; j < els.length; j++) {
+            els[j].addEventListener("click", function(event){
+                var el = event.target;
+                console.log(el);
+                var param = el.getAttribute('class');
+                if (el.getAttribute("active") === 'yes') {return}
+                var carId = el.parentNode.parentNode.getAttribute('data-car-id');
+                var data = el.textContent;
+                var form = '<form class="form-inline-car-update" method="post" action="/cars/updateOwn">';
+                form +='<input name="id" id="id" value="'+ carId +'" type="hidden">';
+                form +='<input name="'+ param +'" id="'+ param +'" value="'+ data +'" type="text">';
+                form +='<input class="button small inline-update-button" value="update" type="submit">';
+                form +='</form>';
+                console.log(form);
+                el.innerHTML = form;
+                el.setAttribute('active','yes');
+            });
+        }
+    }
 
 }
