2567d763a521a1e7bd0d507fe0eb410404ba1cfc
Mirolim Mirzakhmedov <mirolim777@gmail.com>
2014-01-12 16:58:58 +0500

    Add more assertions to ClientOwnDataUpdate test

--- tests/acceptance/ClientOwnDataUpdateCept.php
+++ tests/acceptance/ClientOwnDataUpdateCept.php
@@ -22,5 +22,38 @@ $I->doubleClick(".inline-update-button");
 //Add wait to process ajax response
 $I->wait(50);
 $I->see("(99890) 124-23-22");
+$I->wantTo('update my email');
+$I->seeElement("span.contactemail");
+$I->doubleClick("span.contactemail");
+$I->fillField("contactemail",'mynew@email.com');
+$I->doubleClick(".inline-update-button");
+//Add wait to process ajax response
+$I->wait(50);
+$I->see("mynew@email.com");
+$I->wantTo('update my info');
+$I->seeElement("span.moreinfo");
+$I->doubleClick("span.moreinfo");
+$I->fillField("moreinfo",'I am adding more info');
+$I->doubleClick(".inline-update-button");
+//Add wait to process ajax response
+$I->wait(50);
+$I->see("I am adding more info");
+$I->wantTo('update my car milage');
+$I->seeElement("span.milage");
+$I->doubleClick("span.milage");
+$I->fillField("milage",'51000');
+$I->doubleClick(".inline-update-button");
+//Add wait to process ajax response
+$I->wait(50);
+$I->see("51000");
+$I->wantTo('update my car daily milage');
+$I->seeElement("span.dailymilage");
+$I->doubleClick("span.dailymilage");
+$I->fillField("dailymilage",'15');
+$I->doubleClick(".inline-update-button");
+//Add wait to process ajax response
+$I->wait(50);
+$I->see("15");
+
 
 
